aaba1ecf341f048ec91b42d042e2c287a45576a2
vember31 <prestigioustennis@gmail.com>
2024-02-20 17:52:04 -0600

    i'm debugging now

--- kubernetes/apps/default/olivetin/app/scripts/blocky-disable-5m.sh
+++ kubernetes/apps/default/olivetin/app/scripts/blocky-disable-5m.sh
@@ -11,33 +11,35 @@ KUBECTL_DIRECTORY=/home/olivetin
 # First check to see if kubectl exists
 echo "Checking for existence of kubectl..."
 
-if [ -e "$KUBECTL_LOCATION" ]; then
+if [ -e "${KUBECTL_LOCATION}" ]; then
     echo "Kubectl exists. Proceeding to Blocky script."
 else
     echo "Kubectl does not exist - downloading."
     curl -LO --output-dir "$KUBECTL_DIRECTORY" https://dl.k8s.io/release/v1.29.2/bin/linux/amd64/kubectl
-    chmod +x $KUBECTL_LOCATION
+    chmod +x ${KUBECTL_LOCATION}
     echo "Kubectl downloaded & executable."
 fi
 
 echo "Starting Blocky Script in $PAUSE_SECONDS ..."
-BLOCKY_PODS=$($KUBECTL_LOCATION get pods -n $NAMESPACE -o=jsonpath="{range .items[*]}{.metadata.name} " -l app.kubernetes.io/name=blocky)
+BLOCKY_PODS=$(${KUBECTL_LOCATION} get pods -n $NAMESPACE -o=jsonpath="{range .items[*]}{.metadata.name} " -l app.kubernetes.io/name=blocky)
+
+echo $BLOCKY_PODS #this is debugging only
 
 sleep "$PAUSE_SECONDS"
 
 for pod in $BLOCKY_PODS; do
     case "${ACTION}" in
         status)
-            $KUBECTL_LOCATION exec -n $NAMESPACE "${pod}" -- /app/blocky blocking status;
+            ${KUBECTL_LOCATION} exec -n $NAMESPACE "${pod}" -- /app/blocky blocking status;
         ;;
         enable)
-            $KUBECTL_LOCATION exec -n $NAMESPACE "${pod}" -- /app/blocky blocking enable;
+            ${KUBECTL_LOCATION} exec -n $NAMESPACE "${pod}" -- /app/blocky blocking enable;
         ;;
         disable)
             if [ -z "${DURATION}" ]; then
-                $KUBECTL_LOCATION exec -n $NAMESPACE "${pod}" -- /app/blocky blocking disable --groups "${BLOCKY_GROUPS}"
+                ${KUBECTL_LOCATION} exec -n $NAMESPACE "${pod}" -- /app/blocky blocking disable --groups "${BLOCKY_GROUPS}"
             else
-                $KUBECTL_LOCATION exec -n $NAMESPACE "${pod}" -- /app/blocky blocking disable --duration "${DURATION}" --groups "${BLOCKY_GROUPS}";
+                ${KUBECTL_LOCATION} exec -n $NAMESPACE "${pod}" -- /app/blocky blocking disable --duration "${DURATION}" --groups "${BLOCKY_GROUPS}";
             fi
         ;;
     esac
