cdecd1dd95963fccec50908a6f0ff9d05cf57af8
vember31 <prestigioustennis@gmail.com>
2024-02-20 17:32:44 -0600

    fix sleep command?

--- kubernetes/apps/default/olivetin/app/scripts/blocky-disable-5m.sh
+++ kubernetes/apps/default/olivetin/app/scripts/blocky-disable-5m.sh
@@ -20,24 +20,24 @@ else
     echo "Kubectl downloaded & executable."
 fi
 
-echo "Starting Blocky Script in ${PAUSE_SECONDS}..."
+echo "Starting Blocky Script in $PAUSE_SECONDS ..."
 BLOCKY_PODS=$($KUBECTL_LOCATION get pods -n $NAMESPACE -o=jsonpath="{range .items[*]}{.metadata.name} " -l app.kubernetes.io/name=blocky)
 
-sleep ${PAUSE_SECONDS}
+sleep "$PAUSE_SECONDS"
 
 for pod in $BLOCKY_PODS; do
     case "${ACTION}" in
         status)
-            ${KUBECTL_LOCATION} exec -n $NAMESPACE "${pod}" -- /app/blocky blocking status;
+            $KUBECTL_LOCATION exec -n $NAMESPACE "${pod}" -- /app/blocky blocking status;
         ;;
         enable)
-            ${KUBECTL_LOCATION} exec -n $NAMESPACE "${pod}" -- /app/blocky blocking enable;
+            $KUBECTL_LOCATION exec -n $NAMESPACE "${pod}" -- /app/blocky blocking enable;
         ;;
         disable)
             if [ -z "${DURATION}" ]; then
-                ${KUBECTL_LOCATION} exec -n $NAMESPACE "${pod}" -- /app/blocky blocking disable --groups "${BLOCKY_GROUPS}"
+                $KUBECTL_LOCATION exec -n $NAMESPACE "${pod}" -- /app/blocky blocking disable --groups "${BLOCKY_GROUPS}"
             else
-                ${KUBECTL_LOCATION} exec -n $NAMESPACE "${pod}" -- /app/blocky blocking disable --duration "${DURATION}" --groups "${BLOCKY_GROUPS}";
+                $KUBECTL_LOCATION exec -n $NAMESPACE "${pod}" -- /app/blocky blocking disable --duration "${DURATION}" --groups "${BLOCKY_GROUPS}";
             fi
         ;;
     esac
